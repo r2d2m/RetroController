b8a8eb9c94aadf68bb7956822b1999e3348704de
Vinicius <vncastanheira@gmail.com>
2019-10-28 11:50:44 -0300

    runtime testing

---
+++ Assets/RetroController/Sample/Scripts/SampleIce.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace vnc.Samples
+{
+    public class SampleIce : MonoBehaviour
+    {
+        RetroController retroController;
+
+        public float iceFriction = 1f;
+
+        void Start()
+        {
+            retroController = FindObjectOfType<RetroController>();
+        }
+
+        public void OnEnterIce()
+        {
+            if (retroController)
+            {
+                //retroController.RuntimeScriptable.GetInt(2);
+                //retroController.RuntimeScriptable.Set("GroundFriction", iceFriction);
+            }
+        }
+
+        public void OnExitIce()
+        {
+            if (retroController)
+            {
+                //retroController.RuntimeScriptable.Reset(() => retroController.Profile.GroundFriction);
+            }
+        }
+    }
+}
---
+++ Assets/RetroController/Sample/Scripts/SampleTeleport.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace vnc.Samples
+{
+    public class SampleTeleport : MonoBehaviour {
+
+        public RetroController retroController;
+
+        public void Teleport()
+        {
+            retroController.TeleportTo(transform.position);
+        }
+    }
+}
