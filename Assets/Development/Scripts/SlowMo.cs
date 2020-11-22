51ded54a1d2b6965410b3fade45ee33a0581715f
Vinicius <Vinicius@VAPOR>
2020-11-22 13:27:52 -0300

    2018 upgrade

---
+++ Assets/Development/Scripts/SlowMo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace vnc.Development
+{
+    public class SlowMo : MonoBehaviour
+    {
+        public float value = 1f;
+
+        void Update()
+        {
+            Time.timeScale = value;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+    }
+}
